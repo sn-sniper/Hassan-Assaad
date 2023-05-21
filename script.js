61a1928c24c36d3b501cbc93e10535d77fef386d
Hassan Assaad <81250610+sn-sniper@users.noreply.github.com>
2023-05-21 03:02:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function(e){
+    e.preventDefault()
+    var name = document.getElementById('Name').value;
+    var email = document.getElementById('Email').value;
+    var subject = document.getElementById('Subject').value;
+    var message = document.getElementById('Message').value;
+Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "assaadhassan00021@gmail.com",
+    Password: "2EF7B124932CCB6AED54644D42993F740DF6",
+    To: 'assaadhassan00021@gmail.com',
+    From: email,
+    Subject: subject,
+    Body: message,
+}).then(
+    message => alert(message)
+);
+})
