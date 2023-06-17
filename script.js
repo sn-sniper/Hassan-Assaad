32e15815af189e27be04b119c02e5574f839d1f5
Hassan Assaad <81250610+sn-sniper@users.noreply.github.com>
2023-06-17 22:45:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ Email.send({
     Host: "smtp.elasticemail.com",
     Username: "assaadhassan00021@gmail.com",
     Password: "2EF7B124932CCB6AED54644D42993F740DF6",
-    To: 'devilsbreath696@gmail.com',
+    To: 'assaadhassan00021@gmail.com',
     From: email,
     Subject: subject,
     Body: message,
